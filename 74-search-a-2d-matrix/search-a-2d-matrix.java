2521706ba538eb66c9429f3a2c435716bbd6166e
Rama Mangtani <mangtanirama14@gmail.com>
2026-07-25 19:21:41 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (80.22%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row=matrix.length;
+        int col=matrix[0].length;
+        int left=0;
+        int right=row*col-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+                int rows=mid/col;
+                int cols=mid%col;
+            
+            if(matrix[rows][cols]==target){
+                return true;
+            }
+            else if(matrix[rows][cols]<target){
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+        }
+        return false;
+    }
+}
