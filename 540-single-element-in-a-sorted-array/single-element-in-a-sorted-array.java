8adb4cbb9883a777eb1478da07bd071b9df61959
Rama Mangtani <mangtanirama14@gmail.com>
2026-07-26 01:59:47 +0530

    Time: 0 ms (100.00%) | Memory: 52.8 MB (78.09%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -4,16 +4,16 @@ class Solution {
         int right=nums.length-1;
         while(left<right){
             int mid=left+(right-left)/2;
-            if(mid%2!=0){
-                mid--;
+            if(mid%2==0){
+                mid++;
             }
-            if(nums[mid]==nums[mid+1]){
-                left=mid+2;
+            if(nums[mid]==nums[mid-1]){
+                left=mid+1;
             }
             else{
-                right=mid;
+                right=mid-1;
             }
         }
-        return nums[left];
+        return nums[right];
     }
 }
