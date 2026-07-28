1253cc1a500913b262ecc8d55c9d477f838d654f
Rama Mangtani <mangtanirama14@gmail.com>
2026-07-29 01:23:30 +0530

    Time: 8 ms (52.47%) | Memory: 44 MB (74.17%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,28 +1,28 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-       int[] freq1=new int[26];
-       int[] freq2=new int[26];
-       if(s1.length()>s2.length()){
+      int[] freq1=new int[26];
+      int[] freq2=new int[26];
+      if(s1.length()>s2.length()){
         return false;
-       }
-       for(int i=0;i<s1.length();i++){
-            char ch=s1.charAt(i);
-            freq1[ch-'a']++;
-       }
-       for(int j=0;j<s1.length();j++){
-            char c=s2.charAt(j);
-            freq2[c-'a']++;
-        } 
-        for(int k=s1.length();k<s2.length();k++){
-            if( Arrays.equals(freq1,freq2)){
-                return true;
-            }
-            else{
-                freq2[s2.charAt(k-s1.length())-'a']--;
-                freq2[s2.charAt(k)-'a']++;
-            }
+      }
+      for(int i=0;i<s1.length();i++){
+        char ch=s1.charAt(i);
+        freq1[ch-'a']++;
+      }
+      for(int j=0;j<s1.length();j++){
+        char ch=s2.charAt(j);
+        freq2[ch-'a']++;
+      }
+      for(int k=s1.length();k<s2.length();k++){
+        char ch=s2.charAt(k);
+        if(Arrays.equals(freq1,freq2)){
+            return true;
         }
-     return Arrays.equals(freq1,freq2);
-
+        else{
+            freq2[s2.charAt(k-s1.length())-'a']--;
+            freq2[ch-'a']++;
+        }
+      }
+      return Arrays.equals(freq1,freq2);
     }
 }
