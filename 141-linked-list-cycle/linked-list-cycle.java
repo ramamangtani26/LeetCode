606b8ffc28330c892666e65c9347518d3f5e845a
Rama Mangtani <mangtanirama14@gmail.com>
2026-08-06 15:04:38 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (16.47%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -14,11 +14,11 @@ public class Solution {
         ListNode slow=head;
         ListNode fast=head;
         while(fast!=null && fast.next!=null){
-            if(fast.next==slow){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(fast==slow){
                 return true;
             }
-            fast=fast.next.next;
-            slow=slow.next;
         }
         return false;
     }
