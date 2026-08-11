caec7cfffeb6b21ae8c7ff12614c207f4b8089bb
Rama Mangtani <mangtanirama14@gmail.com>
2026-08-11 14:07:46 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (20.59%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -11,18 +11,17 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
       ListNode dummy=new ListNode(-1);
-      ListNode current =dummy;
+      ListNode current=dummy;
       while(l1!=null && l2!=null){
-      if(l1.val<=l2.val){
-        current.next=l1;
-        l1=l1.next;
-       
-      }
-      else{
-        current.next=l2;
-        l2=l2.next;
-      }
-       current=current.next;
+        if(l1.val<=l2.val){
+            current.next=l1;
+            l1=l1.next;
+        }
+        else{
+            current.next=l2;
+            l2=l2.next;
+        }
+        current=current.next;
       }
       if(l1!=null){
         current.next=l1;
@@ -30,6 +29,6 @@ class Solution {
       if(l2!=null){
         current.next=l2;
       }
-    return dummy.next;
+      return dummy.next;
     }
 }
