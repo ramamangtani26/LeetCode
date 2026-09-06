901fbac9ce06101d93baa0301ee589cdca08d792
Rama Mangtani <mangtanirama14@gmail.com>
2026-09-06 18:35:14 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (12.36%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -7,11 +7,11 @@ class Solution {
             if(nums[mid]==target){
                 return mid;
             }
-            else if(nums[mid]<target){
-                left=mid+1;
+            else if(nums[mid]>target){
+                right--;
             }
             else{
-                right=mid-1;
+                left++;
             }
         }
         return left;
