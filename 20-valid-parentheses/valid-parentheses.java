bd24877e832b9264c57adafaae64c42bf3efbe2f
Rama Mangtani <mangtanirama14@gmail.com>
2026-09-06 12:35:20 +0530

    Time: 4 ms (36.47%) | Memory: 43.5 MB (25.61%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,30 +1,34 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack=new Stack<>();
-       
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-            
-           
-            if(ch=='{' ||ch=='(' ||ch=='['){
+            if(ch=='('||ch=='{'||ch=='['){
                 stack.push(ch);
             }
-            
-            else {
-                if(stack.isEmpty()){
-                    return false;
-                }
-                char top=stack.peek();
-            if((ch=='}' && top=='{') ||
-            ( ch==')' && top=='(') ||(ch==']' && top=='[')){
-               stack.pop();
-            }
-            else{
+            else if(stack.isEmpty()){
                 return false;
             }
             
+            
+            else {
+                if(!stack.isEmpty()){
+                    char top=stack.peek();
+                    if((ch==')'&& top=='(')||
+                    (ch=='}' && top=='{')||
+                    (ch==']' && top=='[')){
+                        stack.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{
+                    return false;
+                }
+               
             }
         }
-        return (stack.isEmpty());
+        return(stack.isEmpty());
     }
 }
