8a38651dac36fbf9be9553c92ab8bcd63d4ba0a7
Rama Mangtani <mangtanirama14@gmail.com>
2026-09-06 12:10:01 +0530

    Time: 1 ms (83.97%) | Memory: 42.7 MB (73.48%) - LeetSync

--- 290-word-pattern/word-pattern.java
+++ 290-word-pattern/word-pattern.java
@@ -1,27 +1,28 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        HashMap<Character,String> map1=new HashMap<>();
-        HashMap<String,Character> map2=new HashMap<>();
-        String[] words=s.split(" ");
-        if(pattern.length()!=words.length){
-            return false;
-        }
-        for(int i=0;i<pattern.length();i++){
-            char ch=pattern.charAt(i);
-            String ch2=words[i];
-            if(map1.containsKey(ch)){
-                if(!map1.get(ch).equals(ch2)){
-                    return false;
-                }
+       HashMap<Character,String> map=new HashMap<>();
+       HashMap<String,Character> map2=new HashMap<>();
+       String[] words=s.split(" ");
+       if(pattern.length()!=words.length){
+        return false;
+       }
+       for(int i=0;i<pattern.length();i++){
+        char ch=pattern.charAt(i);
+        String k=words[i];
+        if(map.containsKey(ch)){
+            if(!map.get(ch).equals(k)){
+                return false;
             }
-            if(map2.containsKey(ch2)){
-                if(!map2.get(ch2).equals(ch)){
-                    return false;
-                }
-            }
-            map1.put(ch,ch2);
-            map2.put(ch2,ch);
         }
-        return true;
+        if(map2.containsKey(k)){
+            if(!map2.get(k).equals(ch)){
+                return false;
+            }
+        }
+        map.put(ch,k);
+        map2.put(k,ch);
+        
+       }
+       return true;
     }
 }
