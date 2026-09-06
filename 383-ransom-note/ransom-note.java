a11a7c5300a329931acfa0b66046a918569e8f95
Rama Mangtani <mangtanirama14@gmail.com>
2026-09-06 11:55:42 +0530

    Time: 16 ms (19.55%) | Memory: 47.2 MB (20.23%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,18 +1,19 @@
 class Solution {
     public boolean canConstruct(String r, String m) {
-        HashMap<Character,Integer>map=new HashMap<>();
-        char[] arr1=r.toCharArray();
-        char[] arr2=m.toCharArray();
-        for(int i=0;i<m.length();i++){
-            map.put(arr2[i],map.getOrDefault(arr2[i],0)+1);
-        }
-        for(int j=0;j<r.length();j++){
-            int count=map.getOrDefault(arr1[j],0);
+       HashMap<Character,Integer> map=new HashMap<>();
+       for(int i=0;i<m.length();i++){
+        char ch=m.charAt(i);
+        map.put(ch,map.getOrDefault(ch,0)+1);
+       }
+       for(int j=0;j<r.length();j++){
+        char ch=r.charAt(j);
+            int count=map.getOrDefault(ch,0);
             if(count==0){
                 return false;
             }
-            map.put(arr1[j],count-1);
-        }
-        return true;
+            map.put(ch,count-1);
+
+       }
+       return true;
     }
 }
